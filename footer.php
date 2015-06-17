7e7bcbf349ae74ffbc965466f4d0ed8c778fc0f1
marion vandewalle <marion@925.ch>
2015-06-17 13:56:16 +0200

    CSS du footer

--- footer.php
+++ footer.php
@@ -8,9 +8,29 @@
  */
 ?>
 	</div><!-- #main -->
-
-	<?php get_sidebar( 'footer' ); ?>
-
+	
+	
+	<div class="colonne1">
+		<p>
+				<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/lesverts.svg" width="25px"/> <strong>Les Verts, mouvement écologiste vaudois</strong>
+			</p>
+				<div class="adresse">
+					<p>
+						<br/>Place Palud 7
+						<br/>1003 Lausanne
+						<br/><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/phone.svg" width="15px"/> 021 351 36 30
+						<br/><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/fax.svg" width="15px"/> 021 351 36 31
+						<br/><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/mail.svg" width="15px"/> verts(at)verts-vd.ch
+						<br/>CCP : 65-90000-6
+					</p>
+				</div>
+	</div>
+	
+	
+	<div class="colonne2">
+		<a href="<?php echo get_stylesheet_directory_uri(); ?>/medias/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/medias.svg" width="300px"/></a>
+	</div>
+	
 	<footer id="colophon" class="site-footer clear" role="contentinfo">
 		<div class="site-info">
 			©<?php echo date('Y');?> Les Verts Vaudois			
