03e32c736d00589573bdfe5e75d0c6451a75ccab
6clicks <john@6clicks.ch>
2015-06-30 14:27:35 +0200

    diver test

--- page-templates/full-width-page.php
+++ page-templates/full-width-page.php
@@ -8,20 +8,25 @@
 get_header(); ?>
 
 	<?php while ( have_posts() ) : the_post(); ?>
-
-	<div class="hero <?php echo forefront_additional_class(); ?>">
-		<?php
+<div class="hero <?php echo forefront_additional_class(); ?>">
+<figure>
+					<?php
 			if ( '' != get_the_post_thumbnail() )
 				the_post_thumbnail( 'forefront-page-thumbnail' );
-		?>
-		<div>
-			<div>
-				<div>
-					<h1 class="page-title"><?php the_title(); ?></h1>
-				</div>
-			</div>
-		</div>
+		else {
+echo '<img src="' . get_bloginfo('stylesheet_directory') . '/img/default.jpg" />';
+} ?>
+					<div>
+						<div>
+							<div>
+								<h1 class="page-title"><?php the_title(); ?></h1>
+							</div>
+						</div>
+					</div>
+				</figure>
+
 	</div>
+
 
 	<?php endwhile; ?>
 
