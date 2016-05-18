69719dc70d41ae13289cd262a11cc270a3437f8b
6clicks <john@6clicks.ch>
2016-05-18 08:30:20 +0200

    Divers petit bug d'affichage ...

--- page-templates/front-page.php
+++ page-templates/front-page.php
@@ -9,7 +9,7 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area front-page-content-area full-width">
+	<div id="primary-home" class="content-area front-page-content-area full-width">
 		<div id="content" class="site-content" role="main">
 
 			<?php while ( have_posts() ) : the_post(); ?>
